58405a1c62cf88d5021a7ee037bf79927686044d
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-10-09 18:47:55 +0330

    remove btn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const add_btn = document.querySelector(".add-new");
 const list = document.querySelector("#list-element");
 const input = document.querySelector(".writing-tasks");
 
-const todo_list = [];
+let todo_list = [ ];
+let nextTodoIdndex= 1;
 
 function  listTasks(title){
     const tasks_list = document.createElement("li");
@@ -15,7 +16,6 @@ function  listTasks(title){
     checkbox.setAttribute("type", "checkbox");
 
     const task = document.createElement("p");
-    // task.classList.add("task");
     task.innerText= title;
 
     const remove = document.createElement("div");
@@ -37,36 +37,53 @@ function syncStorage(title){
     localStorage.setItem('my_list', string_list);
 }
 
+let index = 0
+function addIndex() {
+    index = nextTodoIdndex++
+}
+
 add_btn.addEventListener("click", ()=> {
     const input_value = input.value;
 
     if (input_value !== '') {
         syncStorage(input_value);
         listTasks(input_value);
-
+        addIndex();
         clearInput();
+        console.log(index)
     }
  
 })
 
-const previous_list = JSON.parse(localStorage.getItem('my_list'));
+// =======Store Tasks========== //
+
+// const previous_list = JSON.parse(localStorage.getItem('my_list'));
 // todo_list = previous_list;
 
-for ( let i=0; i < previous_list.length; i++){
-    const title = previous_list[i];
-    listTasks(title);
-}
-
-// remove tasks //
-// const removeBtn = document.querySelector(".remove-icon");
-
-// function remove() {
-//     todo_list.splice();
+// for ( let i=0; i < previous_list.length; i++){
+//     const title = previous_list[i];
+//     listTasks(title);
 // }
 
-// removeBtn.addEventListener("click", ()=>{
-//     remove();
-// })
+
+// ========== remove tasks =============//
+
+const removeBtn =document.querySelector(".remove-icon");
+
+removeBtn.addEventListener("click", ()=>{
+    console.log("remove")
+    todo_list.splice(index, 1);
+    nextTodoIdndex= nextTodoIdndex-1
+})
+
+// for (let i = 0; i < removeBtn.length ; i++) {
+//     removeBtn[i].addEventListener("click", ()=>{
+//         todo_list.splice(index, 1);
+//         nextTodoIdndex= nextTodoIdndex-1
+//     })
+    
+// }
+
 
 
 ///// ---------------- Date ---------------- //////
@@ -84,16 +101,25 @@ screen_date.innerText = currentDate();
 
 // -------------- if task cheched ------------- ////
 
-const checkBox = document.querySelector("input[type='checkbox']")
+const checkBox = document.querySelectorAll("input[type='checkbox']");
 let checked = false; 
 
-checkBox.addEventListener("click", ()=>{
-    task = document.querySelector("p")
-    console.log(task)
-    
-    if (!checked) {
-        task.classList.toggle("checked-terms")
-    }
+for (let i = 0; i < checkBox.length; i++) {
+    checkBox[i].addEventListener ("click" , ()=>{
+        console.log(i)
+        if (!checked){
+            const task = document.querySelectorAll('p');
+            for (let j = 0; j < checkBox.length; j++) {
+                task[j].classList.toggle("checked-terms");
+                console.log(task[j])
+            }
+        }
+    })
+}
 
-})
+
+
+
+
+
 
