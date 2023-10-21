a6d412c11b11fb537f8f1f1d68f3370d2a181527
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-10-21 22:34:34 +0330

    adding delete all and remove functionality

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const add_btn = document.querySelector(".add-new");
 const list = document.querySelector("#list-element");
 const input = document.querySelector(".writing-tasks");
+const deleteAll = document.querySelector("#delete-all");
 
-let todo_list = [ ];
-let nextTodoIdndex= 1;
+const STORAGE_KEY = "my_list"
 
-function  listTasks(title){
+let todo_list = [];
+
+// ======= Work with DOM ======== //
+
+function renderTasks(item) {
     const tasks_list = document.createElement("li");
     tasks_list.classList.add("tasks-list");
     
@@ -14,12 +18,24 @@ function  listTasks(title){
 
     const checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
+    // if (item.status) {
+    //     checkbox.setAttribute("checked", "true")
+    // }
+    checkbox.checked = item.status
+
+    checkbox.addEventListener("click", ()=>{
+        toggleStatus(item.title)
+    })
 
     const task = document.createElement("p");
-    task.innerText= title;
+    task.innerText= item.title;
 
-    const remove = document.createElement("div");
+    const remove = document.createElement("span");
     remove.classList.add("remove-icon");
+
+    remove.addEventListener("click", ()=>{
+        removeTask(item.title)
+    })
 
     task_container.append(checkbox, task);
     tasks_list.append(task_container, remove);
@@ -27,66 +43,144 @@ function  listTasks(title){
     list.append(tasks_list);
 }
 
+function renderList(){
+    // Remove old items:
+    list.innerHTML = "";
+
+    // Render items :
+    /* Model 1:
+     for ( let i=0; i < todo_list.length; i++){
+        const title = todo_list[i];
+        renderTasks(title);
+    } */
+
+    //  Model 2:
+    todo_list.forEach((item) => {
+        renderTasks(item);
+    })
+
+}
+
 function clearInput(){
     input.value = '';
 } 
 
-function syncStorage(title){
-    todo_list.push(title);
+
+// =======Load tasks from storage========== //
+
+function syncStorage(){
     const string_list = JSON.stringify(todo_list);
-    localStorage.setItem('my_list', string_list);
+    localStorage.setItem(STORAGE_KEY, string_list);
 }
 
-let index = 0
-function addIndex() {
-    index = nextTodoIdndex++
+function loadFromStorage(){
+    let listFromStorage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    todo_list = listFromStorage;
 }
-
-add_btn.addEventListener("click", ()=> {
-    const input_value = input.value;
-
-    if (input_value !== '') {
-        syncStorage(input_value);
-        listTasks(input_value);
-        addIndex();
-        clearInput();
-        console.log(index)
-    }
  
-})
+//  ======= Functionality ======== //
 
-// =======Store Tasks========== //
+function toggleStatus(title) {
+    // Medel 1:  
+    // for (let i = 0; i < todo_list.lenght; i++) {
+    //     const list_item = todo_list[i];
 
-// const previous_list = JSON.parse(localStorage.getItem('my_list'));
-// todo_list = previous_list;
+    //     if (list_item.title === title){
+    //        todo_list.status = !todo_list.status
+    //     }
+        
+    // }
 
-// for ( let i=0; i < previous_list.length; i++){
-//     const title = previous_list[i];
-//     listTasks(title);
-// }
+    todo_list.forEach((item)=>{
+        if(item.title === title){
+            item.status = !item.status
+        }
+    })
+
+    syncStorage();
+}
+ 
+function addItem(item) {
+    const next_item = {
+        title: item.title,
+        status: item.status,
+    }
+    todo_list.push(next_item);
+}
+
+// ========== remove tasks ============= //
+
+function removeTask(title) {
+    //MODEL 1: for (let i = 0; i < todo_list.length ; i++) {
+    //     const list_item = todo_list[i];
+
+    //     if (list_item.title === title){
+    //         todo_list.splice(i, 1);
+    //     }
+    // }
+    todo_list.forEach((item, i)=>{
+        if (item.title === title) {
+            todo_list.splice(i, 1);
+        }
+    })
+    syncStorage();
+    renderList();
+}
+
+function onDeleteAll(){ 
+    todo_list = todo_list.filter((item) => {
+        // return item.status === true ? false : true ;
+        // or:
+        return !item.status;
+    })
+
+    syncStorage();
+    renderList();
+}
+
+//  ======= Run the App ======= //
+function onAddItem() {
+    const input_value = input.value;
+    // if(input_value === ''){
+    //     alert("You Should Write a title!")
+    //     return;
+    // }else {
+    //     const item = {
+    //         title: input_value,
+    //         status: false,
+    //     }
+    //     addItem(item);
+    //     syncStorage();
+    //     renderTasks(item);
+    //     clearInput();
+    // }
+    // Model2:
+    if (input_value !== '') {
+        const item = {
+            title: input_value,
+            status: false,
+        }
+        addItem(item);
+        syncStorage();
+        renderTasks(item);
+        clearInput();
+    }
+}
+
+function events() {
+    add_btn.addEventListener("click", onAddItem)
+    deleteAll.addEventListener("click", onDeleteAll)
+}
+
+function init() {
+    loadFromStorage();
+    renderList();
+    events();
+}
 
 
-// ========== remove tasks =============//
 
-const removeBtn =document.querySelector(".remove-icon");
-
-removeBtn.addEventListener("click", ()=>{
-    console.log("remove")
-    todo_list.splice(index, 1);
-    nextTodoIdndex= nextTodoIdndex-1
-})
-
-// for (let i = 0; i < removeBtn.length ; i++) {
-//     removeBtn[i].addEventListener("click", ()=>{
-//         todo_list.splice(index, 1);
-//         nextTodoIdndex= nextTodoIdndex-1
-//     })
-    
-// }
-
-
-
-///// ---------------- Date ---------------- //////
+///// ---------------- App Date ---------------- //////
 
 function currentDate() {
     const month =  ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
@@ -101,22 +195,26 @@ screen_date.innerText = currentDate();
 
 // -------------- if task cheched ------------- ////
 
-const checkBox = document.querySelectorAll("input[type='checkbox']");
-let checked = false; 
+// const checkBox = document.querySelectorAll("input[type='checkbox']");
+// let checked = false; 
 
-for (let i = 0; i < checkBox.length; i++) {
-    checkBox[i].addEventListener ("click" , ()=>{
-        console.log(i)
-        if (!checked){
-            const task = document.querySelectorAll('p');
-            for (let j = 0; j < checkBox.length; j++) {
-                task[j].classList.toggle("checked-terms");
-                console.log(task[j])
-            }
-        }
-    })
-}
+// for (let i = 0; i < checkBox.length; i++) {
+//     checkBox[i].addEventListener ("click" , ()=>{
+//         console.log(i)
+//         if (!checked){
+//             const task = document.querySelectorAll('p');
+//             for (let j = 0; j < checkBox.length; j++) {
+//                 task[j].classList.toggle("checked-terms");
+//                 console.log(task[j])
+//             }
+//         }
+//     })
+// }
 
+
+// =========== init ==============  //
+
+init()
 
 
 
